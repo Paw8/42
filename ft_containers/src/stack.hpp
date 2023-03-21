b1c95a921e0cfe38ae9f157e47bbc8b7f9798293
Pedro <dasilva.pedro187@gmail.com>
2023-03-21 18:21:28 +0100

    rename

--- ft_containers/src/stack.hpp
+++ ft_containers/src/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: pcordeir <pcordeir@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 10:42:50 by pcordeir          #+#    #+#             */
-/*   Updated: 2023/01/18 14:57:23 by pcordeir         ###   ########.fr       */
+/*   Updated: 2023/03/21 18:20:51 by pcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,26 +24,26 @@ namespace ft {
 			typedef	typename container_type::size_type		size_type;
 
 
-		explicit stack (const container_type& ctnr = container_type()) : s_container(ctnr) {}
+		explicit stack (const container_type& ctnr = container_type()) : c(ctnr) {}
 
 		~stack() {}
 
-		bool					empty(void) const { return (s_container.size() == 0); }
+		bool					empty(void) const { return (c.size() == 0); }
 
-		size_type				size(void) const { return (s_container.size()); }
+		size_type				size(void) const { return (c.size()); }
 
-		value_type&				top(void) { return (s_container.back()); }
+		value_type&				top(void) { return (c.back()); }
 
-		const value_type&		top(void) const { return (s_container.back()); }
+		const value_type&		top(void) const { return (c.back()); }
 
-		void					push(const value_type& val) { s_container.push_back(val); }
+		void					push(const value_type& val) { c.push_back(val); }
 
-		void					pop(void) { s_container.pop_back(); }
+		void					pop(void) { c.pop_back(); }
 
-		const container_type	base(void) const { return s_container; }
+		const container_type	base(void) const { return c; }
 
 		private:
-			container_type	s_container;
+			container_type	c;
 
 	};
 
